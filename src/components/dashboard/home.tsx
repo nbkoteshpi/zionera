8f9f6ca3b09ea75d7e031e2a3bbac6766a3dbbb0
NB Kotesh <brahma.koteswara@upgrad.com>
2024-09-26 09:09:07 +0530

    login form created

--- src/components/dashboard/home.tsx
+++ src/components/dashboard/home.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { BrowserRouter as  Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home1 from "./home1";
 import AppComponent1 from "../../AppComponent1";
+import Login2 from "../common/user/login2";
 
 const Home = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home1 />} />
-        <Route path='/Home' element={<Home1 />} />
-        <Route path='/Test' element={<AppComponent1 />} />
+        <Route path="/" element={<Login2 />} />
+        <Route path="/Home" element={<Home1 />} />
+        <Route path="/Test" element={<AppComponent1 />} />
       </Routes>
     </Router>
   );
