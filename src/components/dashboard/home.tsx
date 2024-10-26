4f2691b49e001e1d28ad4dc81bca19f6d1d11551
Nannepamu, Dhanakar Babu Export License Required - US Collins <E4058100@utcain.com>
2024-10-26 12:40:16 +0530

    Added Register Details

--- src/components/dashboard/home.tsx
+++ src/components/dashboard/home.tsx
@@ -3,13 +3,16 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home1 from "./home1";
 import AppComponent1 from "../../AppComponent1";
 import Login2 from "../common/user/login2";
-
+import Register from "../common/user/register";
+import Register1 from "../common/user/register1";
 const Home = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login2 />} />
-        <Route path="/Home" element={<Home1 />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/register1" element={<Register1 />} />
+        <Route path="/Home" element={<Home />} />
         <Route path="/Test" element={<AppComponent1 />} />
       </Routes>
     </Router>
